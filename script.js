18d04ab120ac2ea73066bbcb3ad3e2967f50ecb4
Xiaoyi Chen <126677643+xxc451@users.noreply.github.com>
2026-01-29 15:50:16 +0100

    Add function to render books and add basic styles

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+
+const body = document.querySelector("body");
+
+function renderLibrary() {
+    for (const book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("card");
+
+        const title = document.createElement("h3");
+        title.textContent = book.title;
+        bookCard.appendChild(title);
+
+        const author = document.createElement("p");
+        author.textContent = `Author: ${book.author}`;
+        bookCard.appendChild(author);
+
+        const pages = document.createElement("p");
+        pages.textContent = `Pages: ${book.pages}`;
+        bookCard.appendChild(pages);
+
+        const status = document.createElement("P");
+        status.textContent = title.read ? "Read" : "Not Read";
+        bookCard.appendChild(status);
+
+        body.appendChild(bookCard);
+    }
+}
+
+addBookToLibrary("The Catcher in the Rye", "J. D. Salinger", 234, true);
+
+renderLibrary()
