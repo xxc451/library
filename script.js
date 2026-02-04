684424599f2cd915f15b9a58baf7834a258f1447
Xiaoyi Chen <126677643+xxc451@users.noreply.github.com>
2026-02-04 19:00:00 +0100

    Refactor object constructor into class

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
